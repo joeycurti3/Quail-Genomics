6167dd95a88b0674e376ba83d0099cab36b17121
joeycurti3 <83435184+joeycurti3@users.noreply.github.com>
2024-09-18 13:37:29 -0700

    Create run_step04_b_CAQU_SeqArray2VCF_20240624.sh

---
+++ scripts/step04_analyses/ADMIXTURE/run_step04_b_CAQU_SeqArray2VCF_20240624.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#$ -l highp,h_data=20G,h_rt=10:00:00
+#$ -wd <insert path to working directory>
+#$ -o <insert log directory path>
+#$ -e <insert log directory path>
+#$ -m bae
+#$ -M 1joeynik
+#$ -N step04_b_CAQU_SeqArray2VCF
+
+## Import Packages
+
+source <insert path to miniconda>
+conda activate my_SNPRelate
+
+set -o pipefail
+
+## Main
+
+Rscript step04_b_CAQU_SeqArray2VCF_20240624.R
+
+## Clean Up
+echo -e "[$(date "+%Y-%m-%d %T")] Done"
+
+conda deactivate
+
