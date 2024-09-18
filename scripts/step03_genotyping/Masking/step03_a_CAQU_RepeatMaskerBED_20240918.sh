25c9e1f4bebc0bc2bd8d676cc105ce3ae1ba7af5
joeycurti3 <83435184+joeycurti3@users.noreply.github.com>
2024-09-18 10:41:37 -0700

    Create step03_a_CAQU_RepeatMaskerBED_20240918.sh

---
+++ scripts/step03_genotyping/Masking/step03_a_CAQU_RepeatMaskerBED_20240918.sh
@@ -0,0 +1,11 @@
+## Create a .bed file from .gff file from RepeatMasker output ## 
+
+# I first sorted the BED file by the first column (scaffold name) and then by the second column (start position):
+
+sort -k 1,1 -k2,2n GCA_023055505.1_bCalCai1.0.p_genomic_mask.bed > GCA_023055505.1_bCalCai1.0.p_genomic_mask_NoHead_sorted.bed
+
+# Then I remove the first two rows that had weirdly named scaffolds:
+
+tail -n +2 GCA_023055505.1_bCalCai1.0.p_genomic_mask_NoHead_sorted.bed > GCA_023055505.1_bCalCai1.0.p_genomic_mask_FINAL.bed
+
+# The resulting .bed file was accepted by GATK 
